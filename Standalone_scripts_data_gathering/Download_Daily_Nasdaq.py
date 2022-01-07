128ea2a1faeb79f183a3d1c3248a3861f5b19d17
Konstantinos Siachras <32716168+consiahras@users.noreply.github.com>
2022-01-07 16:39:57 +0100

    Update Download_Daily_Nasdaq.py
    When it downloads the daily Nasdaq file, it will also delete the older versions of file so it wont fill space with useles files.

--- Standalone_scripts_data_gathering/Download_Daily_Nasdaq.py
+++ Standalone_scripts_data_gathering/Download_Daily_Nasdaq.py
@@ -1,6 +1,17 @@
 import wget
 import time
+import os
 
+dest_dir='/home/ksiachras/Daily_nasdaq_file'
 timestr= time.strftime("%Y%m%d")
+now = time.time()
 ftp_url='ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
-wget.download(ftp_url, out=("nasdaqlisted" + timestr))
+wget.download(ftp_url, out= dest_dir)
+
+#Remove the older files that downloaded before that are before 5min timestamp
+print("Removing Older Files except the last file we downloaded")
+for file in os.listdir(dest_dir):
+    pathfile=os.path.join(dest_dir, file)
+    if pathfile.startswith("nasdaq"):
+        if os.stat(pathfile).st_mtime < now - 300:
+            os.remove(os.path.join(dest_dir, file))
